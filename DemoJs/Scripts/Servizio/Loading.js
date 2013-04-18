51b4ea7e15d5178da4bed3b9b630583d5699dfdd
Gabri <gabriele.bonzi@studiofarma.it>
2013-04-18 18:03:22 +0200

    Demo JS

---
+++ DemoJs/Scripts/Servizio/Loading.js
@@ -0,0 +1,18 @@
+﻿function openModalLoading(idModalLoading) {
+    $("#" + idModalLoading).modal('show');
+}
+
+function closeModalLoading(idModalLoading) {
+    setTimeout(function () {
+        $("#" + idModalLoading).modal('hide');
+    }, 500);
+}
+
+function closeModalLoading(idModalLoading, func) {
+    setTimeout(function () {
+        $("#" + idModalLoading).modal('hide');
+        if (func) {
+            func();
+        }
+    }, 500);
+}
