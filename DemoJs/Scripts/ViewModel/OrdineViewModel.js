78f67294829deb38dc085de2966538477b1b1d86
Gabri <gabriele.bonzi@studiofarma.it>
2013-04-18 17:02:49 +0200

    [WBC] Demo JS

---
+++ DemoJs/Scripts/ViewModel/OrdineViewModel.js
@@ -0,0 +1,34 @@
+﻿define("OrdineViewModel",
+    function () {
+
+        //#region Costruttore modello SchedaIpossemica
+        var ViewModelConstructor = function () {
+            var self = this;
+
+            //#region Proprietà categoria
+
+            //#region Observable
+            self.modelCarrello = ko.observable([]);
+            //#endregion
+
+            //#endregion
+
+            //#region Computed
+            self.IsOrdinabile = ko.computed(function () {
+                return self.modelCarrello().IsOrdinabile();
+            });
+            //#endregion
+
+            //#region Funzioni
+            self.bindOrdine = function () {
+            };
+            self.cercaProdotto = function () {
+            };
+            self.ordinaCarrello = function() {
+            };
+           //#endregion
+        };
+
+        return ViewModelConstructor;
+        //#endregion
+    });
---
+++ DemoJs/Scripts/ViewModel/ProdottoViewModel.js
@@ -0,0 +1,30 @@
+﻿define("ProdottoViewModel",
+    function () {
+
+        //#region Costruttore modello SchedaIpossemica
+        var ViewModelConstructor = function () {
+            var self = this;
+
+            //#region Proprietà categoria
+            self.Codice = '';
+            self.Descrizione = '';
+            self.Ditta = '';
+            //#region Observable
+            self.qta = ko.observable(0);
+            //#endregion
+
+            //#endregion
+
+            //#region Computed
+
+            //#endregion
+
+            //#region Funzioni
+            self.svuotaProdotto = function () {
+            };
+            //#endregion
+        };
+
+        return ViewModelConstructor;
+        //#endregion
+    });
